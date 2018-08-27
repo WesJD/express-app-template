b9f5e1f717ff091f4477bacb8ab23adb2a95e1dd
Wesley Smith <me@wesjd.net>
2018-08-27 17:07:52 -0400

    add dev mode

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ import routes from "./routes/registry"
 
 const config = require("../config.json")
 const debug = getDebugger("server")
-const clientDirectory = __dirname + "/../../client/bin"
+const clientDirectory = getViewsDirectory()
 
 export const server = express()
 
@@ -16,3 +16,13 @@ server.use(express.static(clientDirectory))
 server.use(routes)
 
 server.listen(config.port, () => debug("Started on port", config.port))
+
+function getViewsDirectory() {
+    let base = __dirname + "/../../client/"
+    if (process.env.NODE_ENV == "production") {
+        base += "bin"
+    } else {
+        base += "src"
+    }
+    return base
+}
